ccb13c8dd7aed14fcdc807c0f3ba4bc151282ea7
MORGANJASON <420morganjason@gmail.com>
2023-03-22 11:03:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // Code your solution in this file!
+ 
+//   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//   console.log(drivers[0], drivers[1]);
+
+// const drivers = ['Antonia', 'Nuru'];
+
+//   console.log(drivers[0], drivers[1])
+
+//   // Last two drivers
+// const returnLastTwoDrivers()
+//   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+//   console.log(drivers[2], drivers[3]);
+
+
+// //Selecting Drivers 
+//  let selectingDrivers = [['returnFirstTwoDrivers()'], ['returnLastTwoDrivers()']] 
+//     return print = ["Antonia"], ["Nuru"];
+//   console.log(selectingDrivers("Antonia", "Nuru"));
+
+
+//   // Fare Multiplier
+//   function createFareMultiplier(num, fn) {
+//     return fn * 4;
+//      const fareMultiplier = createFareMultiplier(2); {
+//           return print = "Function"
+//      }
+  
+//   }console.log(createFareMultiplier("4", "8"));
+
+
+// // Fare Quantupler
+// function createFareQuantupler(fare) { 
+//   return fare * 5;
+//    const fareQuantupler = createFareQuantupler(2); {
+//     return print = "function"
+//    }
+//    }
+//    console.log(createFareQuantupler(6));
+
+// // Fare doubler
+//    function fareDoubler(fare) {
+//     return fare * 2;
+//     const fareDoubler = fareDoubler(2); {
+//       return print = "function"
+//     }
+//    }
+//    console.log(fareDoubler(10));
+
+
+//   //  Fare tripler
+//    function fareTripler(fare) {
+//     return fare * 3;
+//     const fareTripler = fareTripler(3); {
+//       return print = "function"
+//     }
+//    }
+//    console.log(fareTripler(12));
+
+const returnFirstTwoDrivers[]
+  const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; {
+ }
+console.log(returnFirstTwoDrivers(driver[0], driver[1]));
+
+
+
+
+// // return first two drivers
+//   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//   console.log(drivers[0], drivers[1]);
+
+
+// // return last two drivers
+//   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//   console.log(drivers[2], drivers[3]);  
+
+
+
+
+  
